0c8e998156fc96ef398b4b4ac54a1f9d49efbcc4
Pavel Butuzov <pavel.butuzov@testit.software>
2022-11-29 16:23:05 +0300

    [Pytest] Supported fixture name in decorators.

--- testgear-python-commons/setup.py
+++ testgear-python-commons/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='testgear-python-commons',
-    version='2.0.6',
+    version='2.0.7',
     description='Python commons for Test Gear',
     url='https://github.com/testgear-tms/adapters-python/',
     author='Integration team',
